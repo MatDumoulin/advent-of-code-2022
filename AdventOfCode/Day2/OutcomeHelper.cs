0338492809825c3a9aff3cbdffeb22f4901f85d4
mathieu.dumoulin <mathieu.dumoulin@outlook.com>
2022-12-04 21:28:05 -0500

    Day 2: Done!

---
+++ AdventOfCode/Day2/OutcomeHelper.cs
@@ -0,0 +1,65 @@
+﻿namespace Day2;
+
+public enum Outcome
+{
+    Draw,
+    Loss,
+    Win
+}
+
+public static class OutcomeHelper
+{
+    public static Outcome GetOutcomeOfRound(char opponentMove, char myMove)
+    {
+        if (opponentMove == 'A' && myMove == 'X'
+            || opponentMove == 'B' && myMove == 'Y'
+            || opponentMove == 'C' && myMove == 'Z')
+        {
+            return Outcome.Draw;
+        }
+
+        if (opponentMove == 'A' && myMove == 'Z'
+            || opponentMove == 'B' && myMove == 'X'
+            || opponentMove == 'C' && myMove == 'Y')
+        {
+            return Outcome.Loss;
+        }
+
+        return Outcome.Win;
+    }
+
+    public static char GetMoveForOutcome(char opponentMove, Outcome desiredOutcome)
+    {
+        if (opponentMove == 'A' && desiredOutcome == Outcome.Loss)
+        {
+            return 'Z';
+        }
+
+        if (opponentMove == 'A' && desiredOutcome == Outcome.Win)
+        {
+            return 'Y';
+        }
+
+        if (opponentMove == 'B' && desiredOutcome == Outcome.Loss)
+        {
+            return 'X';
+        }
+
+        if (opponentMove == 'B' && desiredOutcome == Outcome.Win)
+        {
+            return 'Z';
+        }
+
+        if (opponentMove == 'C' && desiredOutcome == Outcome.Loss)
+        {
+            return 'Y';
+        }
+
+        if (opponentMove == 'C' && desiredOutcome == Outcome.Win)
+        {
+            return 'X';
+        }
+
+        return (char)(opponentMove + 23);
+    }
+}
---
+++ AdventOfCode/Day2/Program.cs
@@ -0,0 +1,53 @@
+﻿using Day2;
+
+var input = File.ReadAllLines(Directory.GetCurrentDirectory() + "/input.txt");
+
+var scorePerMove = new Dictionary<char, int>
+{
+    { 'X', 1 },
+    { 'Y', 2 },
+    { 'Z', 3 },
+};
+
+var scorePerOutcome = new Dictionary<Outcome, int>
+{
+    { Outcome.Loss, 0 },
+    { Outcome.Draw, 3 },
+    { Outcome.Win, 6 },
+};
+
+var letterToOutcome = new Dictionary<char, Outcome>
+{
+    { 'X', Outcome.Loss },
+    { 'Y', Outcome.Draw },
+    { 'Z', Outcome.Win },
+};
+
+var totalScore = 0;
+
+foreach (var line in input)
+{
+    var opponentMove = line[0];
+    var myMove = line[2];
+
+    var outcome = OutcomeHelper.GetOutcomeOfRound(opponentMove, myMove);
+    totalScore += scorePerMove[myMove] + scorePerOutcome[outcome];
+}
+
+Console.WriteLine($"Answer 1: {totalScore}");
+
+//////////////////////////////////////////////
+
+totalScore = 0;
+
+foreach (var line in input)
+{
+    var opponentMove = line[0];
+    var desiredOutcome = letterToOutcome[line[2]];
+
+    var myMove = OutcomeHelper.GetMoveForOutcome(opponentMove, desiredOutcome);
+    totalScore += scorePerMove[myMove] + scorePerOutcome[desiredOutcome];
+}
+
+Console.WriteLine($"Answer 2: {totalScore}");
+
