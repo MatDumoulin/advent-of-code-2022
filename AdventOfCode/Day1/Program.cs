53a8870cb5053730740b8669fab11b9b16fd3797
mathieu.dumoulin <mathieu.dumoulin@outlook.com>
2022-12-04 20:51:13 -0500

    Initial commit

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadAllLines(Directory.GetCurrentDirectory() + "/input.txt");
+var elvesInventory = new List<int>();
+var current = 0;
+
+foreach (var line in input)
+{
+    if (line == string.Empty)
+    {
+        elvesInventory.Add(current);
+        current = 0;
+    }
+    else
+    {
+        current += Int32.Parse(line);
+    }
+}
+        
+elvesInventory.Sort((a,b) => b.CompareTo(a));
+var maxOfTop3 = elvesInventory.Take(3).Sum();
+        
+Console.WriteLine($"Answer: {maxOfTop3}");
